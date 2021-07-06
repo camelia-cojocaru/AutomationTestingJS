ff2eaf0637031bb85b2001d281ee085a733fa984
Camelia Cojocaru <camelia.cojocaru@assist.ro>
2021-07-06 11:06:35 +0300

    [ADD] Readme text file

---
+++ readme.txt
@@ -0,0 +1,3 @@
+Run: npx wdio run wdio.conf.js (this creates the allure-results folder)
+Create report:  allure generate allure-results --clean -o allure-report (created the allure-report folder)
+Open report: allure open (starts the web server of the report)
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,3 +1,7 @@
+const drivers = {
+    chrome: { version: '91.0.4472.101' }, // https://chromedriver.chromium.org/
+}
+
 exports.config = {
     //
     // ====================
@@ -19,7 +23,7 @@ exports.config = {
     specs: ['./tests/*.js'],
     // Patterns to exclude.S
     exclude: [
-        './tests/Test-SignIn.js'
+        
     ],
     //
     // ============
@@ -104,7 +108,13 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['chromedriver'],
+    services: [
+        ['selenium-standalone', {
+            logPath: 'logs',
+            installArgs: { drivers }, // drivers to install
+            args: { drivers } // drivers to use
+        }]
+    ],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
